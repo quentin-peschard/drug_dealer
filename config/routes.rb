ca2ad8a125f257930923bf5aa45e392a01c6a316
marly teixeira <teixeiramg.marly@gmail.com>
2021-08-23 18:02:08 +0200

    adding the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pharmacies, only: [:index, :show]
+  resources :orders, only: [:index, :edit, :update] do
+    resources :ordered_drugs, only: [:index]
+  end
 end
