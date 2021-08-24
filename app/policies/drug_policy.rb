2adf9bbb4134a74a28aa0593fe6a2a591b22d968
Quentin Peschard <qpeschard49@gmail.com>
2021-08-24 16:51:13 +0200

    changed policy

--- app/policies/drug_policy.rb
+++ app/policies/drug_policy.rb
@@ -1,18 +1,7 @@
 class DrugPolicy < ApplicationPolicy
-<<<<<<< drug_view
   class Scope < Scope
     def resolve
       scope.all
     end
   end
 end
-=======
-    def resolve
-        scope.all
-    end
-  
-    def index?
-        true
-    end
-end
->>>>>>> master
