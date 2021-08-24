11fcc7a202b057e09d4fea059721379548a0f426
Quentin Peschard <qpeschard49@gmail.com>
2021-08-24 14:23:08 +0200

    seed change

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'faker'
 gem 'pundit'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
+gem 'nokogiri', '~> 1.8'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,15 +17,12 @@ require 'json'
 data = JSON.parse(File.read(Rails.root.join('db/drugs.json')))
 
 data.each do |drug|
-    Drug.create!(
-        name: drug["drug_name"],
-        description: drug["description"],
-        prescripted: false
-    )
+  Drug.create!(name: drug['drug_name'],
+               description: drug['description'],
+               prescripted: false)
 end
-customer = User.create({first_name: 'Mike', last_name: 'Jones', email: 'mike@hello.com', password: '123456' })
-pharmacy = Pharmacy.create({name: 'Pharm', address: '123 hello', description: 'very nice', user_id: 1 })
-drug = Drug.create({name: 'drug', prescribed: false})
-order = Order.create({user_id: 1, total: 45.3, pharmacy_id: 1, status: 'pending', accepted: false})
-ordered_drug = OrderedDrug.create({order_id: 1, price: 10, quantity: 2, drug_id: 1})
-
+customer = User.create({ first_name: 'Mike', last_name: 'Jones', email: 'mike@hello.com', password: '123456' })
+pharmacy = Pharmacy.create({ name: 'Pharm', address: '123 hello', description: 'very nice', user_id: 1 })
+drug = Drug.create({ name: 'drug', prescripted: false })
+order = Order.create({ user_id: 1, total: 45.3, pharmacy_id: 1, status: 'pending', accepted: false })
+ordered_drug = OrderedDrug.create({ order_id: 1, price: 10, quantity: 2, drug_id: 1 })
