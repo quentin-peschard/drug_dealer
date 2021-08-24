6be3685fe5dba87fb2566b21083852711fd31d5a
Quentin Peschard <qpeschard49@gmail.com>
2021-08-24 15:10:55 +0200

    problem map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_100821) do
+ActiveRecord::Schema.define(version: 2021_08_24_091126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_100821) do
   create_table "drugs", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.boolean "prescripted"
+    t.boolean "prescribed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
