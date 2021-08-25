fc78d1bccb642b51ff39430892b3ea5d30b30ab7
Quentin Peschard <qpeschard49@gmail.com>
2021-08-25 16:32:56 +0200

    finished index

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -23,7 +23,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-  map.fitBounds(bounds, { padding: 40, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 50, maxZoom: 15 });
 };
 
 
