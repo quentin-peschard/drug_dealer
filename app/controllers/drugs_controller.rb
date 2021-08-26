ef159b208daee72b63983ffe002ac3e6b0897c6e
carole valentin <valentincarole25@gmail.com>
2021-08-26 15:24:57 +0200

    searchbar

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -5,6 +5,12 @@ class DrugsController < ApplicationController
     @drugs = policy_scope(Drug)
     order_oui_non = current_user.orders.where(pharmacy_id: params[:pharmacy_id], status: "Order in Progress").first
 
+    if params[:query].present? #ajouté pour le PG SEARCH
+      @drugs = Drug.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @drugs = policy_scope(Drug)
+    end
+
     if order_oui_non
       @order = order_oui_non
     else
@@ -15,6 +21,7 @@ class DrugsController < ApplicationController
   private
 
   def retrieve_order
+    @pharmacy = params[:pharmacy_id]
     begin
       @order = Order.find(user: current_user, pharmacy: Pharmacy.find(params[:pharmacy_id]))
     rescue
