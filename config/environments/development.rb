e1348c2fd1525c5e683c4ed0cf52156c3457d3c4
marly teixeira <teixeiramg.marly@gmail.com>
2021-08-30 10:50:24 +0200

    adding ngrok to mobile app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # Ngrok for pwa web mobile app
-  config.hosts << /[a-z0-9]+\.ngrok\.io/
+  config.hosts << /[a-z0-9\W]+\.ngrok\.io/
 end
