c13da038fb70bcf4c9b196cb4f089faf534f3d9d
Quentin Peschard <qpeschard49@gmail.com>
2021-08-31 14:31:26 +0200

    commented js

--- app/javascript/controllers/list_controller.js
+++ app/javascript/controllers/list_controller.js
@@ -1,20 +1,21 @@
-import { csrfToken } from "@rails/ujs";
-import { Controller } from "stimulus"
+// import { csrfToken } from "@rails/ujs";
+// import { Controller } from "stimulus"
 
-export default class extends Controller {
-  static target = ['card'];
+// export default class extends Controller {
+//   static target = ['card'];
 
-  addDrug(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    console.log(event.currentTarget);
-    // fetch(currentElement.href, {
-    //   headers: { 'Accept': 'application/json' }
-    // })
-    //   .then(response => response.json())
-    //   .then((data) => {
-    //     const targetedElement = document.querySelector(currentElement.dataset.drugQuantity);
-    //     targetedElement.innerText = data.quantity;
-    //   })
-  }
-}
+//   addDrug(event) {
+//     event.stopPropagation();
+//     event.preventDefault();
+//     console.log(event.currentTarget);
+//     // fetch(event.currentTarget.href, {
+//     //   method: 'POST',
+//     //   headers: { 'Accept': "application/json", 'X-CSRF-Token': csrfToken() },
+//     //   body: new FormData(this.formTarget)
+//     // })
+//     //   .then(response => response.json())
+//     //   .then((data) => {
+//     //     console.log(data)
+//     //   });
+//   }
+// }
