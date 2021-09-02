94d7003fefa6acb123291b3c66575029055d3aef
marly teixeira <teixeiramg.marly@gmail.com>
2021-09-02 10:38:09 +0200

    fixing missing call cart to ajax

--- app/controllers/ordered_drugs_controller.rb
+++ app/controllers/ordered_drugs_controller.rb
@@ -24,11 +24,16 @@ class OrderedDrugsController < ApplicationController
 
   def create
     @ordered_drug = OrderedDrug.new(drug: Drug.find(params[:drug_id]), order: Order.find(params[:order_id]), user: current_user)
+    @pharmacy = Pharmacy.find(Order.find(params[:order_id]).pharmacy.id)
     authorize @ordered_drug
     if @ordered_drug.save!
       respond_to do |format|
         format.html { redirect_to drugs_path(params: { pharmacy_id: params[:pharmacy_id] }) }
-        format.json { render json: { html: render_to_string(partial: 'drugs/drugs_list.html', locals: { drugs: Drug.all })}}
+        format.json { render json: { 
+                                    html: render_to_string(partial: 'drugs/drugs_list.html', locals: { drugs: Drug.all }), 
+                                    ordered_drugs_count: OrderedDrug.joins(:order).where(orders: { user: current_user, status: 'initialized', pharmacy: @pharmacy }).count
+                                    }
+                    }
       end
       
     else
--- app/javascript/controllers/list_controller.js
+++ app/javascript/controllers/list_controller.js
@@ -23,6 +23,7 @@ export default class extends Controller {
     .then((data) => {
         this.drugListTarget.innerHTML = data.html;
         document.querySelector('.overlay').classList.add("hidden");
+        document.querySelector('.action-bar').insertAdjacentHTML('beforeend', `<div class='total'>${data.ordered_drugs_count}</div>`)
         exitThing();
         addDrug();
     })
